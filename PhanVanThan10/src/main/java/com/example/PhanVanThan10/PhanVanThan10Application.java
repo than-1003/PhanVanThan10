a4260ad5013d8e4339453bc8202954e69a05501d
Than <thanp1451@gmail.com>
2026-03-26 14:23:33 +0700

    Initial commit

---
+++ PhanVanThan10/src/main/java/com/example/PhanVanThan10/PhanVanThan10Application.java
@@ -0,0 +1,13 @@
+package com.example.PhanVanThan10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanVanThan10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanVanThan10Application.class, args);
+	}
+
+}
---
+++ PhanVanThan10/src/test/java/com/example/PhanVanThan10/PhanVanThan10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhanVanThan10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanVanThan10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
